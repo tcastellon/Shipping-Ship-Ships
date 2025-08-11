5be7e8d32fbcdb2897fd441a7faaf10527668cbf
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-11 13:28:03 -0500

    Updated docks module

--- scripts/Docks.js
+++ scripts/Docks.js
@@ -1,5 +1,5 @@
 //Import function from database that gets a copy of docks data array
-import { getDocks } from "./database.js"
+import { getDocks, getHaulingShips } from "./database.js"
 
 //Export and define the function that is responsible for generating the HTML string for the DOM
 export const dockList = () => {
@@ -10,10 +10,51 @@ export const dockList = () => {
 
     for (const dock of docks) {
         //Convert each dock object to an <li> and append to the docksHTML string
-        docksHTML += `<li>${dock.location}</li>`
+        docksHTML += `<li data-type="dock"
+                          data-id="${dock.id}"
+                          data-location="${dock.location}"
+                          >${dock.location}</li>`
     }
 
     docksHTML += "</ul>"
 
     return docksHTML
 }
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        //Was the dock list item clicked?
+        if (itemClicked.dataset.type === "dock") {
+            //Get the id of the dock that was clicked on
+            const dockId = itemClicked.dataset.id
+            let foundAMatch = false
+            let dockAlertMessage = `${itemClicked.dataset.location} dock is currently unloading `
+
+            const haulers = getHaulingShips()
+            //Iterate all of the hauling ships
+            for (const hauler of haulers) {
+                //Does the dockId foreign key match the dock id
+                if (parseInt(dockId) === hauler.dockId) {
+                    //Are there multiple hauling ships in the dock? one? none?
+                    dockAlertMessage += `${hauler.name} `
+                    foundAMatch = true  
+                } 
+                
+            }
+            //Should say nothing when none of the hauler dockIds match the dock id of the item clicked
+            if (!foundAMatch) { 
+                dockAlertMessage += "nothing."}
+            window.alert(`${dockAlertMessage}`)
+    
+            //Window alert showing the users the haulers being serviced by the dock that was clicked on
+            
+        }
+        
+
+
+
+
+    }
+)
